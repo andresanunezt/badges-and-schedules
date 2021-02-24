88c940a8b9ef0394f9607e6a4bf9bd7c9a37fbf7
Andrés Nuñez <andresanunez98@gmail.com>
2021-02-24 16:29:22 -0500

    commit tree 6c42d6b1c8333880cc8d5377325344dd86a2ee35

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    name_and_message = []
+    names.each do |name|
+    name_and_message <<  "Hello, my name is #{name}."
+    end
+name_and_message
+end
+
+def assign_rooms(speakers)
+   rooms_and_speaker = []
+    speakers.each.with_index(1) do |name, index|
+     rooms_and_speaker << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    rooms_and_speaker
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badges|
+puts badges
+    end
+    assign_rooms(attendees).each do |room_assignments|
+puts room_assignments
+    end
+end
